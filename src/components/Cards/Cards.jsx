ddf5d1c1c9ccd1ccbbff2a841e30c90c436ebdb9
miseljicAleksa <aleksa.miseljic.etna@gmail.com>
2020-04-21 22:42:15 +0200

    Add Chart component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -36,7 +36,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom> infected</Typography>
+                        <Typography color="textSecondary" gutterBottom> deaths </Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={deaths.value} duration={2.5} separator=","/>
                         </Typography>
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect  } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData.length
+            ? (
+                <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: 'Infected',
+                        borderColor: '#4b6716',
+                        fill: true
+                    }, {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: 'Died',
+                        borderColor: '#a00c2f',
+                        backgroundColor: '#a00c2f7f',
+                        fill: true
+                    }]
+                    }  
+                    }
+                />
+            ) : null
+    );
+
     return(
-        <div>
-            <h1>Chart</h1>
+        <div className={styles.container}>
+            { lineChart }
         </div>
     )
 }
