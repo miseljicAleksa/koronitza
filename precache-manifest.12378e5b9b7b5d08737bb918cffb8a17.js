802fa573ed455a393e4d331f0fd54ce4c8386c4b
miseljicAleksa <aleksa.miseljic.etna@gmail.com>
2020-04-22 12:21:49 +0200

    Updates

---
+++ precache-manifest.12378e5b9b7b5d08737bb918cffb8a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c3c30c2ae7f3ccaaa97656dccc651d",
+    "url": "/koronitza/index.html"
+  },
+  {
+    "revision": "f2acfa76a63bf48de638",
+    "url": "/koronitza/static/css/main.d723e090.chunk.css"
+  },
+  {
+    "revision": "0291e2faa8682ade92f8",
+    "url": "/koronitza/static/js/2.72070690.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/koronitza/static/js/2.72070690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2acfa76a63bf48de638",
+    "url": "/koronitza/static/js/main.2915d2c0.chunk.js"
+  },
+  {
+    "revision": "27f9667b36e6a85f7d9a",
+    "url": "/koronitza/static/js/runtime-main.3f4ff930.js"
+  }
+]);
